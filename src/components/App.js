325a9c8aa081e662dff0ae9baffcf0824e0642e1
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-11-01 15:33:31 +0200

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [image, setImage] = useState("");
+  useEffect(
+    () =>
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then((r) => r.json())
+        .then((r) => setImage(r)),
+    []
+  );
+
+  return image ? (
+    <img src={image.message} alt="A Random Dog" />
+  ) : (
+    <p>Loading...</p>
+  );
+}
+
+export default App;
